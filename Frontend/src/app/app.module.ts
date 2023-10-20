c490637aed72819e902316fda68e3af5e28dfa1d
harish426 <harish.jamallamudi@slu.com>
2023-10-19 20:32:19 -0500

    commiting after added modules to show post

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
