cbcf66629ebe516fed16f527b7ad02a9f58ffa77
harish426 <harish.jamallamudi@slu.com>
2023-10-20 22:42:08 -0500

    reviewed feed component css and deleted post component as commented

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -7,14 +7,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { PostComponent } from './post/post.component';
+import { FeedComponent } from './feed/feed.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
-    PostComponent
+    FeedComponent
   ],
   imports: [
     BrowserModule,
