fa062cc6b5b1e7f240fc52e27735d01b9ab9c556
udkreddy <udkr1996@gmail.com>
2023-10-29 19:00:42 -0500

    Revamp auth flow

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { FeedComponent } from './feed/feed.component';
+import { AuthGuard } from './helpers/auth.guard';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
-  { path: 'home', component: HomeComponent },
-  { path: 'feed', component: FeedComponent },
+  { path: 'feed', component: FeedComponent, canActivate: [AuthGuard] },
+  { path: '**', redirectTo: 'feed' }
 ];
 
 @NgModule({
