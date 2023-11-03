5ba4440deba19682f428e8c8d4179e9e968e71a6
harish426 <harish.jamallamudi@slu.com>
2023-11-02 23:58:42 -0500

    ts file and frontend part for showing results completed

--- Frontend/src/app/detect/detect.component.ts
+++ Frontend/src/app/detect/detect.component.ts
@@ -29,8 +29,40 @@ export class DetectComponent {
   onFileSelected(event: any) {
     this.selectedFile = event.target.files[0];
   }
-  onSubmit(event: Event){
+  onSubmit(event: Event) {
+    event.preventDefault(); // Prevent the default form submission
 
+    if (this.selectedFile) {
+      const formData = new FormData();
+      formData.append('image', this.selectedFile);
+      console.log(formData)
+      this.http.post<any>('http://127.0.0.1:5000/submit', formData).subscribe(
+        (response:any) => {
+          this.result = response.result;
+          this.picture_error = false;
+          this.response_got=true;
+          console.log(this.result)
+          this.title = this.result.title;
+      this.description = this.result.description;
+      this.prevent = this.result.prevent;
+      this.image_url = this.result.image_url;
+      this.supplement_name = this.result.supplement_name;
+      this.supplement_image_url = this.result.supplement_image_url;
+      this.supplement_buy_link = this.result.supplement_buy_link;
+
+      // Now you can use these variables as needed in your component
+      
+        },
+        (error:any) => {
+          this.picture_error = true;
+          this.response_got=false;
+          setTimeout(() => {
+            this.result = 'please upload the picture of crop leaf';
+          }, 4000);
+        }
+      );
+       // Simulating a 2-second delay (replace with actual detection logic)
+    }
   }
   
 }
