c06470e8589af384f729eb8a99aa09682d221827
balamurali0532 <balamuralisatyendra.bokka@slu.edu>
2023-11-09 21:48:33 -0600

    design patterns

---
+++ Backend/Iterator.js
@@ -0,0 +1,16 @@
+class Iterator {
+    constructor(objects) {
+        this.objects = objects;
+        this.index = 0;
+    }
+
+    next() {
+        if (this.index < this.objects.length) {
+            return { done: false, value: this.objects[this.index++] };
+        } else {
+            return { done: true };
+        }
+    }
+}
+
+module.exports = Iterator;
