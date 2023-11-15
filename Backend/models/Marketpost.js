8b3f46fefa473e1135ec2a99a81d828ca94c87de
Warren Huang <warrenhuang@WarrendeMacBook-Pro.local>
2023-11-14 22:50:13 -0600

    backent schema for marketpost

---
+++ Backend/models/Marketpost.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+
+/**
+ * Marketpost Schema for User Post
+ * Which contains the username,title,content,image,createAt,yeat_of_purchase, address,
+ * city, state, country
+ */
+const MarketpostSchema = new mongoose.Schema({
+username:{
+    type: String,
+    required: true
+},
+title:{
+    type: String,
+    required: true
+},
+content:{
+    type: String,   
+    required: true
+},
+image:[{
+    type: String,   
+    required: false
+}],
+createdAt:{
+    type: Number,
+    default: Date.now,  
+    required: true
+},
+year_of_purchase:{
+    type: Number,
+    default: Date.now,  
+    required: true
+},
+address: {
+    type: String,
+    required: true
+},
+city: {
+    type: String,
+    required: true
+},
+state: {
+    type: String,
+    required: true
+},
+country: {
+    type: String,
+    required: true
+}
+});
+
+const Marketpost = mongoose.model('Marketpost', MarketpostSchema);
+
+module.exports = Marketpost;
