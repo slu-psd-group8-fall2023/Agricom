5c9beb8d27805a7afd0de527e5405661ea432e44
balamurali0532 <balamuralisatyendra.bokka@slu.edu>
2023-11-16 22:44:38 -0600

    Review changes

--- Backend/marketuserpost.js
+++ Backend/marketuserpost.js
@@ -1,30 +1,35 @@
 const User = require('./models/User');
 const Marketpost = require('./models/Marketpost');
-const Iterator = require('./Iterator');
 
 /**
  * Function for market user posts
  */
 async function marketcreatePost(req, res) {
     try {
-        const { username, title, content, image,createdAt, year_of_purchase, address, city, state, country } = req.body;
+        // Destructuring the required fields from the request body
+        const { username, title, content, image, createdAt, year_of_purchase, address, city, state, country } = req.body;
 
-        if (Object.keys(req.body).length === 0) {
-            return res.status(400).json({ error: 'Incomplete data. Please provide all required fields.' });
+        // Checking the individual fields 
+        if (!isValidData({ username, title, content, createdAt, year_of_purchase, address, city, state, country })) {
+            return res.status(400).json({ error: 'Invalid data. Please provide valid data in fields.' });
         }
 
-        if(!isValidDate(createdAt)){
-            return res.status(400).json({ error: 'Invalid date. Please provide valid data in fields.' });
+        // Validate the format of the provided date
+        if (!isValidDate(createdAt)) {
+            return res.status(400).json({ error: 'Invalid date. Please provide a valid date in the createdAt field.' });
         }
 
+        // Find the user in the database based on the provided username
         const user = await User.findOne({
-            username: username
+            username
         });
 
+        // Check if the user exists
         if (!user) {
             return res.status(400).json({ error: 'User not found.' });
         }
 
+        // Create a new market post instance with the provided data
         const newMarketPost = new Marketpost({
             username,
             title,
@@ -38,20 +43,28 @@ async function marketcreatePost(req, res) {
             country
         });
 
+        // Save the new market post to the database
         await newMarketPost.save();
 
         res.status(201).json({ message: 'Market Post created successfully', newMarketPost });
     } catch (error) {
-        console.log(error);
+        // Log any errors that occur during the process
+        console.error(error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
 }
 
-
+// Function to check if a given date string is a valid date
 function isValidDate(dateString) {
     const date = new Date(dateString);
     return !isNaN(date.getTime());
-  }
+}
+
+// Function to check if a given date string is a valid data
+function isValidData(data) {
+    return Object.values(data).every(value => value !== undefined && value !== '');
+}
+
 
 module.exports ={ 
     marketcreatePost,
