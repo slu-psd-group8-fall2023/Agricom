5f7b2be4eae8963cafbbee7825086b55f147db24
balamurali0532 <balamuralisatyendra.bokka@slu.edu>
2023-11-16 22:25:15 -0600

    API to create post in marketplace

--- Backend/marketuserpost.js
+++ Backend/marketuserpost.js
@@ -14,7 +14,7 @@ async function marketcreatePost(req, res) {
         }
 
         if(!isValidDate(createdAt)){
-            return res.status(400).json({ error: 'Invalid data. Please provide valid data in fields.' });
+            return res.status(400).json({ error: 'Invalid date. Please provide valid data in fields.' });
         }
 
         const user = await User.findOne({
@@ -47,89 +47,12 @@ async function marketcreatePost(req, res) {
     }
 }
 
-/**
- * Function to retrieve market posts and sort them by createdAt 
- */
-async function retrieveMarketPosts(req, res) {
-    try {
-        // Find all market posts and sort by createdAt in descending order
-        const marketPosts = await Marketpost.find().sort({ createdAt: 'desc' });
-
-        const marketPostIterator = new Iterator(marketPosts);
-
-        const result = [];
-        let marketPost = marketPostIterator.next();
-        while (!marketPost.done) {
-            result.push(marketPost.value);
-            console.log(marketPost);
-            marketPost = marketPostIterator.next();
-        }
-
-        res.status(200).json({ marketPosts: result });
-
-    } catch (error) {
-        console.error('Error retrieving market posts:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-}
-
-/**
- * Function to handle city filtering on users post
- */
-async function filterMarketPosts(req, res) {
-    try {
-        const { city, state } = req.body;
-
-         // If neither city nor state is provided, return a specific message
-        if (!city && !state) {
-            return res.status(200).json({ message: 'No posts found. Please provide city or state parameters.' });
-        }
-
-        let query = {};
-    
-        // Check if the city parameter is provided
-        if (city) {
-          // Use case-insensitive regex for partial matching
-            query.city = new RegExp(city, 'i');
-        }
-    
-        // Check if the state parameter is provided
-        if (state) {
-          // Use case-insensitive regex for partial matching
-            query.state = new RegExp(state, 'i');
-        }
-
-      // Find market posts based on the query and sort by createdAt in descending order
-        const marketPosts = await Marketpost.find(query).sort({ createdAt: 'desc' });
-
-        const marketPostIterator = new Iterator(marketPosts);
-
-    const result = [];
-    let marketPost = marketPostIterator.next();
-    while (!marketPost.done) {
-        result.push(marketPost.value);
-        marketPost = marketPostIterator.next();
-    }
-
-    if (result.length === 0) {
-        return res.status(200).json({ message: 'No posts found on the provided criteria.' });
-    }
-
-    res.status(200).json({ marketPosts: result });
-
-    } catch (error) {
-        console.error('Error retrieving market posts:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-}
-
 
 function isValidDate(dateString) {
     const date = new Date(dateString);
     return !isNaN(date.getTime());
   }
 
-module.exports ={ marketcreatePost,
-    retrieveMarketPosts,
-    filterMarketPosts,
+module.exports ={ 
+    marketcreatePost,
     };
