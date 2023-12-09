0ce19669fbafcc953ce0167af7d64a1fd25213ab
udkreddy <udkr1996@gmail.com>
2023-12-08 22:59:31 -0600

    Minor fix

--- start.sh
+++ start.sh
@@ -2,4 +2,5 @@ echo "Starting AgriCom"
 cd Frontend && ng build
 echo "build completed"
 cd ../
-npm run start:backend & sudo npm run start:frontend
+npm run start:backend & 
+cd Frontend && http-server dist/agri-com
